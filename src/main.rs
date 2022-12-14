e0351707eb3758e15e32adf9a2dab1ce79e4ffc1
Thamognya Kodi <contact@thamognya.com>
2022-12-14 17:14:58 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+impl collatz {
+    fn general(i: u128) {
+        let mut path: String = String::from("");
+
+    }
+    fn bfs(i: u128) {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut num: u128 = 1;
+    let mut storage: Vec<String> = Vec::new();
+    loop {
+        collatz::general(num);
+        num += 1;
+        println!("{}", num);
+        if num == 10 {
+            break;
+        }
+    }
 }
