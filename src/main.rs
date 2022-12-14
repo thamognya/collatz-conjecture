87600a140660dc7c63b4a7110e5e9888af07e1c4
Thamognya Kodi <contact@thamognya.com>
2022-12-14 17:36:27 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-impl collatz {
+use std::fs;
+use std::path::Path;
+
+pub impl collatz {
     fn general(i: u128) {
         let mut path: String = String::from("");
     }
@@ -10,11 +13,19 @@ fn main() {
     let mut num: u128 = 1;
     let mut storage: Vec<String> = Vec::new();
     loop {
-        collatz::general(num);
+        // collatz::general(num);
+
         num += 1;
-        println!("{}", num);
+        storage.push(num.to_string());
+
         if num == 10 {
             break;
+        }
+
+        if num % 1000 == 0 {
+            storage.rev();
+            fs::write(Path::new("./stofrage.text"), storage).unwrap();
+            storage.rev();
         }
     }
 }
